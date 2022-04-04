b8ef78dfc59adbb2201358a32f7227c3bdd6ec56
Kostya <aktybaeff@yandex.ru>
2022-04-04 17:56:35 +0500

    first commit

---
+++ src/main/java/com/example/demo/NameController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServlet;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.read.ListAppender;
+
+@EnableAutoConfiguration
+@RestController
+public class NameController extends HttpServlet{
+	String rez="";
+	ArrayList<Student> list=new ArrayList<Student>();
+	
+	@RequestMapping("/")
+	public String home(){
+		return "домашняя страница";
+	}
+
+	@RequestMapping(value="/names/players/add",method = RequestMethod.POST)
+	public Student putStudent(@RequestBody Student student){
+		list.add(student);
+		System.out.println("Добавлен студент "+student.name);
+		return student;
+	}
+	
+	@RequestMapping("/names/players/list")
+	public ArrayList<Student> getStudents(){
+		System.out.println("тест метода");
+		return list;
+	}
+}
